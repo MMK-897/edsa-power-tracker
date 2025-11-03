72c8bb9973031631fd069f6886f4cabc2e9fc0e8
Mohamed Kolleh Mansaray <mohamedkolleh20@gmail.com>
2025-11-03 14:05:03 -0800

    Made some changes in the vite config file and the gitignore file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/edsa-power-tracker/', // Add this line - matches your repo name
 })
